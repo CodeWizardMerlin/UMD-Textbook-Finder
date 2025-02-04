3b19620b2660d2175e0f0f4caee9c092f978b5d5
CodeWizardMerlin <zklossner@gmail.com>
2025-02-03 23:50:08 -0500

    Course info page finished

--- textbook_finder.py
+++ textbook_finder.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-print("Paste URL")
-
+enter = "u'\ue007'"
 parse_start = "courseData="
 parse_end = "&utm_source"
-#user_url = input()
-user_url = "https://umcp.bncollege.com/course-material-listing-page?utm_campaign=storeId=15551_langId=-1_courseData=CMSC_216_0401_202501%7CCMSC_250_0303_202501%7CHIST_187_0102_202501%7CMATH_240_0332_202501&utm_source=wcs&utm_medium=registration_integration"
+
+print("Paste URL")
+user_url = input()
+if user_url == "test":
+    user_url = "https://umcp.bncollege.com/course-material-listing-page?utm_campaign=storeId=15551_langId=-1_courseData=CMSC_216_0401_202501%7CCMSC_250_0303_202501%7CHIST_187_0102_202501%7CMATH_240_0332_202501&utm_source=wcs&utm_medium=registration_integration"
+
 parsed_url = user_url[user_url.index(parse_start) + len(parse_start) : user_url.index(parse_end)]
 classes = parsed_url.split("%7C")
 class_array = []
@@ -21,29 +24,54 @@ driver = webdriver.Firefox()
 driver.get("https://umcp.bncollege.com/course-material/course-finder")
 driver.set_window_size(1920, 1500)
 driver.execute_script("document.body.style.transform='scale(.5)';")
-sleep(1)
+sleep(.5)
 
+#add another course button
 for i in range(len(class_array) - 4):
     driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div/div/div[4]/div[2]/form/div/div[3]/div[1]/a[1]").click()
 
-print(class_array)
-enter = "u'\ue007'"
 for i in range(len(class_array)):
     index = i + 2
 
+    #term
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)" 
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
-    sleep(.1)
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
-    sleep(1)
+    #department
+    sleep(.2)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
     char_array = list(class_array[i][0])
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
-    sleep(.3)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     textbox.send_keys(char_array[0]) 
     textbox.send_keys(char_array[1])
     textbox.send_keys(char_array[2])
     textbox.send_keys(char_array[3])
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
+
+    #course number
+    sleep(.2)
+    css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)"
+    driver.find_element(By.CSS_SELECTOR, css_sel).click()
+    textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
+    char_array = list(class_array[i][1])
+    textbox.send_keys(char_array[0]) 
+    textbox.send_keys(char_array[1])
+    textbox.send_keys(char_array[2])
+    driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
+
+    #section
+    sleep(.2)
+    css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
+    driver.find_element(By.CSS_SELECTOR, css_sel).click()
+    textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
+    char_array = list(class_array[i][2])
+    textbox.send_keys(char_array[0]) 
+    textbox.send_keys(char_array[1])
+    textbox.send_keys(char_array[2])
+    textbox.send_keys(char_array[3])
+    driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
+
+#retrieve materials button
+driver.find_element(By.CSS_SELECTOR, "div.bned-campus-form:nth-child(1) > div:nth-child(4) > div:nth-child(2) > a:nth-child(1)").click()
