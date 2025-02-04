ad78f6474ee707f1300ec94623cf682f85f63f1c
CodeWizardMerlin <zklossner@gmail.com>
2025-02-03 19:00:14 -0500

    Half done with navigating the UMD book website

--- textbook_finder.py
+++ textbook_finder.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 print("Paste URL")
 
@@ -19,10 +20,30 @@ for c in classes:
 driver = webdriver.Firefox()
 driver.get("https://umcp.bncollege.com/course-material/course-finder")
 driver.set_window_size(1920, 1500)
-driver.execute_script("document.body.style.transform='scale(.6)';")
+driver.execute_script("document.body.style.transform='scale(.5)';")
+sleep(1)
 
-for i in range(10):
+for i in range(len(class_array) - 4):
     driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div/div/div[4]/div[2]/form/div/div[3]/div[1]/a[1]").click()
 
-driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div/div/div[4]/div[2]/form/div/div[2]/div[2]/div[1]/div/div/span[1]/span[1]/span").send_keys(u'\ue007')
-driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div/div/div[4]/div[2]/form/div/div[2]/div[2]/div[1]/div/div/span[1]/span[1]/span").send_keys(u'\ue007')
+print(class_array)
+enter = "u'\ue007'"
+for i in range(len(class_array)):
+    index = i + 2
+
+    css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)" 
+    driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
+    sleep(.1)
+    driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
+
+    sleep(1)
+    css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
+    char_array = list(class_array[i][0])
+    driver.find_element(By.CSS_SELECTOR, css_sel).click()
+    sleep(.3)
+    textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
+    textbox.send_keys(char_array[0]) 
+    textbox.send_keys(char_array[1])
+    textbox.send_keys(char_array[2])
+    textbox.send_keys(char_array[3])
+    driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
