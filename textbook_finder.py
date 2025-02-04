4bf87113f51f80abc1497b12800f15d46336dfb0
CodeWizardMerlin <zklossner@gmail.com>
2025-02-04 11:52:17 -0500

    Changed interaction timings for consistancy

--- textbook_finder.py
+++ textbook_finder.py
@@ -7,9 +7,8 @@ parse_start = "courseData="
 parse_end = "&utm_source"
 
 print("Paste URL")
-user_url = input()
-if user_url == "test":
-    user_url = "https://umcp.bncollege.com/course-material-listing-page?utm_campaign=storeId=15551_langId=-1_courseData=CMSC_216_0401_202501%7CCMSC_250_0303_202501%7CHIST_187_0102_202501%7CMATH_240_0332_202501&utm_source=wcs&utm_medium=registration_integration"
+user_url = "https://umcp.bncollege.com/course-material-listing-page?utm_campaign=storeId=15551_langId=-1_courseData=CMSC_216_0401_202501%7CCMSC_250_0303_202501%7CHIST_187_0102_202501%7CMATH_240_0332_202501&utm_source=wcs&utm_medium=registration_integration"
+#user_url = input()
 
 parsed_url = user_url[user_url.index(parse_start) + len(parse_start) : user_url.index(parse_end)]
 classes = parsed_url.split("%7C")
@@ -22,27 +21,28 @@ for c in classes:
 
 driver = webdriver.Firefox()
 driver.get("https://umcp.bncollege.com/course-material/course-finder")
-driver.set_window_size(1920, 1500)
-driver.execute_script("document.body.style.transform='scale(.5)';")
+driver.set_window_size(1920, 1500) # large y axis stops bugs with trying to interact with buttons offscreen
+driver.execute_script("document.body.style.transform='scale(.5)';") # keeps all buttons on screen better
 sleep(.5)
 
-#add another course button
+# add another course button
 for i in range(len(class_array) - 4):
     driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div/div/div[4]/div[2]/form/div/div[3]/div[1]/a[1]").click()
 
 for i in range(len(class_array)):
     index = i + 2
 
-    #term
+    # term
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)" 
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
-    #department
-    sleep(.2)
+    # department
+    sleep(.3)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
     char_array = list(class_array[i][0])
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
+    sleep(.1)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     textbox.send_keys(char_array[0]) 
     textbox.send_keys(char_array[1])
@@ -50,10 +50,11 @@ for i in range(len(class_array)):
     textbox.send_keys(char_array[3])
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
-    #course number
-    sleep(.2)
+    # course number
+    sleep(.3)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)"
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
+    sleep(.1)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     char_array = list(class_array[i][1])
     textbox.send_keys(char_array[0]) 
@@ -61,10 +62,11 @@ for i in range(len(class_array)):
     textbox.send_keys(char_array[2])
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
-    #section
-    sleep(.2)
+    # section
+    sleep(.3)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
+    sleep(.1)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     char_array = list(class_array[i][2])
     textbox.send_keys(char_array[0]) 
@@ -73,5 +75,8 @@ for i in range(len(class_array)):
     textbox.send_keys(char_array[3])
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
-#retrieve materials button
+# retrieve materials button
 driver.find_element(By.CSS_SELECTOR, "div.bned-campus-form:nth-child(1) > div:nth-child(4) > div:nth-child(2) > a:nth-child(1)").click()
+
+##################
+
