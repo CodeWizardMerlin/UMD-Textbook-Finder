2f467c4cad5fc3ef026488ae3e2f04b52051c61a
CodeWizardMerlin <zklossner@gmail.com>
2025-02-04 18:12:40 -0500

    Grabs all ISBN and stores

--- textbook_finder.py
+++ textbook_finder.py
@@ -8,7 +8,7 @@ parse_end = "&utm_source"
 
 print("Paste URL")
 user_url = "https://umcp.bncollege.com/course-material-listing-page?utm_campaign=storeId=15551_langId=-1_courseData=CMSC_216_0401_202501%7CCMSC_250_0303_202501%7CHIST_187_0102_202501%7CMATH_240_0332_202501&utm_source=wcs&utm_medium=registration_integration"
-#user_url = input()
+user_url = input()
 
 parsed_url = user_url[user_url.index(parse_start) + len(parse_start) : user_url.index(parse_end)]
 classes = parsed_url.split("%7C")
@@ -29,6 +29,8 @@ sleep(.5)
 for i in range(len(class_array) - 4):
     driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div/div/div[4]/div[2]/form/div/div[3]/div[1]/a[1]").click()
 
+t = .8 
+
 for i in range(len(class_array)):
     index = i + 2
 
@@ -38,11 +40,10 @@ for i in range(len(class_array)):
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
     # department
-    sleep(.3)
+    sleep(t)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
     char_array = list(class_array[i][0])
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
-    sleep(.1)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     textbox.send_keys(char_array[0]) 
     textbox.send_keys(char_array[1])
@@ -51,10 +52,9 @@ for i in range(len(class_array)):
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
     # course number
-    sleep(.3)
+    sleep(t)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)"
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
-    sleep(.1)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     char_array = list(class_array[i][1])
     textbox.send_keys(char_array[0]) 
@@ -63,10 +63,9 @@ for i in range(len(class_array)):
     driver.find_element(By.CSS_SELECTOR, css_sel).send_keys(enter)
 
     # section
-    sleep(.3)
+    sleep(t)
     css_sel = f"div.bned-register-section:nth-child({index}) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(2)"
     driver.find_element(By.CSS_SELECTOR, css_sel).click()
-    sleep(.1)
     textbox = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
     char_array = list(class_array[i][2])
     textbox.send_keys(char_array[0]) 
@@ -78,5 +77,12 @@ for i in range(len(class_array)):
 # retrieve materials button
 driver.find_element(By.CSS_SELECTOR, "div.bned-campus-form:nth-child(1) > div:nth-child(4) > div:nth-child(2) > a:nth-child(1)").click()
 
-##################
 
+sleep(2.5) #webpage load
+
+# ISBN 13
+list = driver.find_elements(By.CLASS_NAME, "value")
+list2 = set()
+for i in list:
+    if len((i.get_attribute("innerText"))) == 13 and i.get_attribute("innerText").isdigit():
+        list2.add(i.get_attribute("innerText"))
